4f393ad8d1a123f7433879b936ab909c4fea4d00
Nazar Kovalchuk <nazark@seekingalpha.com>
2022-12-18 11:47:52 +0100

    Add final main logic

--- app/parser.rb
+++ app/parser.rb
@@ -2,6 +2,12 @@ require 'json'
 require 'date'
 
 class Parser
+  GRANULARITY_MAP = {
+    weekly: 7,
+    monthly: 30,
+    quarterly: 91
+  }
+
   def initialize(order_dir: :asc, granularity: :daily, date: {})
     @order_dir = order_dir
     @granularity = granularity
@@ -9,14 +15,25 @@ class Parser
   end
 
   def perform
-    p sorted_dailies_date_price
-    p sorted_dailies_date_price.size
+    p granulated_date_price
   end
 
   private
 
+  def granulated_date_price
+    return sorted_dailies_date_price if @granularity === :daily
+
+    sorted_dailies_date_price.each_slice(GRANULARITY_MAP[:weekly]).with_object([]) do |slice, result|
+      granularity_val = GRANULARITY_MAP[:weekly] || slice.size
+      granularity_date = "#{slice.first[:date]}..#{slice.last[:date]}"
+      granularity_price = slice.inject(0.0) { |sum, day| sum + day[:price]} / granularity_val
+
+      result << [granularity_date, granularity_price.round(2)]
+    end
+  end
+
   def sorted_dailies_date_price
-    result = dailies_date_price_in_range.sort_by { |_, val| val }
+    result = dailies_date_price_in_range.sort_by { |day_value| day_value[:date] }
     return result if @order_dir === :asc
 
     result.reverse
@@ -26,7 +43,7 @@ class Parser
     parsed_file.each_with_object([]) do |day_value, result|
       next unless present?(day_value['price(USD)']) && (filter_date_from..filter_date_to).include?(day_value['date'])
 
-      result << { date: day_value['date'], price: day_value['price(USD)'] }
+      result << { date: day_value['date'], price: day_value['price(USD)'].to_f }
     end
   end
 
@@ -47,4 +64,4 @@ class Parser
   end
 end
 
-Parser.new(order_dir: :desc).perform
+Parser.new(order_dir: :desc, granularity: :weekly).perform
